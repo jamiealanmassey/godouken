1597a79095a5296cf3f69ce8e44f0eba08662bef
Jamie Massey <jamiealanmassey@gmail.com>
2020-12-29 18:35:50 +0000

    added skeleton code for constructing and storing a data model in memory/files

---
+++ godouken_data_model.cpp
@@ -0,0 +1,17 @@
+/* godouken_data_model.cpp */
+
+#include "godouken_data_model.h"
+
+void GodoukenDataModel::node_index_refresh(GodoukenDataNodeBase *p_node) {}
+void GodoukenDataModel::node_index_insert(GodoukenDataNodeBase *p_node) {}
+
+void GodoukenDataModel::script_parse(const String &p_script_file) {}
+
+void GodoukenDataModel::model_serialise(const String &p_file_name, const String &p_file_location) {}
+void GodoukenDataModel::model_deserialise(const String &p_file_name, const String &p_file_location) {}
+
+GodoukenDataNodeBase *GodoukenDataModel::get_node_root() { return nullptr; }
+GodoukenDataNodeBase *GodoukenDataModel::get_node_from_indexer(const String &p_entry_name) { return nullptr; }
+
+GodoukenDataModel::GodoukenDataModel() {}
+GodoukenDataModel::~GodoukenDataModel() {}
---
+++ godouken_data_model.h
@@ -0,0 +1,40 @@
+/* godouken_data_model.h */
+
+#include "godouken_data_types.h"
+
+#include "core/ustring.h"
+#include "core/object.h"
+#include "core/map.h"
+
+#ifndef GODOUKEN_DATA_MODEL_H
+#define GODOUKEN_DATA_MODEL_H
+
+typedef Map<String, GodoukenDataNodeBase*> GodoukenIndexer;
+
+class GodoukenDataModel : public Object {
+    GDCLASS(GodoukenDataModel, Object);
+
+protected:
+    uint32_t model_script_hash;
+    GodoukenDataNodeBase *model_node_root;
+    GodoukenIndexer model_node_indices;
+
+public:
+    void node_index_refresh(GodoukenDataNodeBase *p_node);
+    void node_index_insert(GodoukenDataNodeBase *p_node);
+
+    void script_parse(const String &p_script_file);
+
+    void model_serialise(const String &p_file_name, const String &p_file_location);
+    void model_deserialise(const String &p_file_name, const String &p_file_location);
+
+    GodoukenDataNodeBase *get_node_root();
+    GodoukenDataNodeBase *get_node_from_indexer(const String &p_entry_name);
+
+public:
+    GodoukenDataModel();
+    ~GodoukenDataModel();
+};
+
+
+#endif // GODOUKEN_DATA_MODEL_H
