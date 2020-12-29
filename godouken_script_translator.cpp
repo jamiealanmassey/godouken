8d469ae2f2baef2df21f8f3903a1bf70b49e2dcd
Jamie Massey <jamiealanmassey@gmail.com>
2020-12-29 18:45:53 +0000

    added skeletal base for the script translator code that will turn GDScripts into relevant data models for exporting HTML, PDF, websites etc.

---
+++ godouken_script_translator.cpp
@@ -0,0 +1,9 @@
+/* godouken_script_translator.cpp */
+
+#include "godouken_script_translator.h"
+#include "godouken_data_model.h"
+
+GodoukenDataModel *GodoukenScriptTranslator::script_translate() { return nullptr; }
+
+GodoukenScriptTranslator::GodoukenScriptTranslator() {}
+GodoukenScriptTranslator::~GodoukenScriptTranslator() {}
---
+++ godouken_script_translator.h
@@ -0,0 +1,30 @@
+/* godouken_script_translator.h */
+
+#include "godouken_data_types.h"
+
+#include "core/object.h"
+
+#ifndef GODOUKEN_SCRIPT_TRANSLATOR_H
+#define GODOUKEN_SCRIPT_TRANSLATOR_H
+
+class GodoukenScriptTranslator : public Object {
+    GDCLASS(GodoukenScriptTranslator, Object);
+
+protected:
+    GDScript* translator_script;
+    int32_t translator_script_line_begin;
+    int32_t translator_script_line_end;
+
+    List<String> reserved_godot_methods;
+    List<String> reserved_godot_types;
+
+public:
+    class GodoukenDataModel *script_translate();
+
+public:
+    GodoukenScriptTranslator();
+    ~GodoukenScriptTranslator();
+};
+
+
+#endif // GODOUKEN_SCRIPT_TRANSLATOR_H
